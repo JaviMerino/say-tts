f4f59d77925f7ce0f9226927eff97fcdcb9b0ca9
Javi Merino <merino.jav@gmail.com>
2025-11-09 16:04:32 +0000

    flake: add an overlay

--- flake.nix
+++ flake.nix
@@ -2,16 +2,21 @@
   description = "say - a TTS CLI tool";
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
-    ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
+    (flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       packages = rec {
         say = pkgs.callPackage ./. {};
         default = say;
       };
-    });
+    }))
+    // {
+      overlays.default = final: prev: {
+        say = self.packages.${prev.stdenv.hostPlatform.system}.default;
+      };
+    };
 }
